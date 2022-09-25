8e6d12691ea4252c50abdb288dc81fe5c25ece26
A007-cloud <dasaditya788@gmail.com>
2022-09-26 00:24:06 +0530

    add get entry point

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.json('Hello World');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
